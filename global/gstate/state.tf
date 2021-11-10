e201cbc538d42c1858ab30d3d5f587e60417c884
Keresifon Ekpenyong <keresifon.isidore@gmail.com>
2021-11-10 21:54:41 +0100

    Initial commit

---
+++ global/gstate/state.tf
@@ -0,0 +1,12 @@
+
+
+terraform {
+  backend "s3" {
+    bucket = "enobasin"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-lock"
+    encrypt = true
+  }
+}
---
+++ global/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = var.s3_bucket
+
+ force_destroy = true
+
+  versioning = {
+    enabled = true
+  }
+
+server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+   
+}
+
+module "dynamodb_table" {
+  source   = "terraform-aws-modules/dynamodb-table/aws"
+
+  name     = var.db_name
+  hash_key = "LockID"
+
+  attributes = [
+    {
+      name = "LockID"
+      type = "S"
+    }
+  ]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "staging"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "enobasin"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-lock"
+    encrypt = true
+  }
+}
